980e8e32d39a9a6a36d2bec8a42c6e92b0f3b33b
מנחם סקלרץ <sklarz@auth.ad.bgu.ac.il>
2017-10-31 09:23:51 +0200

    fixed cross-referenceing issue

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -300,4 +300,5 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'https://docs.python.org/': None,
+                       'neatseq_flow_modules':('http://neatseq-flow-modules.readthedocs.io/en/latest/', None)}
